71e276ae5c8f52a1aa35da24eada1d8bc78f20a0
hunter <huntcsg@gmail.com>
2026-04-13 05:14:01 -0400

    Update formula to v0.3.0

--- Formula/git-worktree-utils.rb
+++ Formula/git-worktree-utils.rb
@@ -12,8 +12,8 @@
 class GitWorktreeUtils < Formula
   desc "Shell utilities for managing git worktrees with bare repo structure"
   homepage "https://github.com/huntcsg/git-worktree-utils"
-  url "https://github.com/huntcsg/git-worktree-utils/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "77abd0d15eba3779efbd15eedcd17865b97ca8d34c8314b0739ba531d0546c80"
+  url "https://github.com/huntcsg/git-worktree-utils/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "584ed4362d3fe6592d9c0e1df1ad134b7108031230349135d3bc3910670b3204"
   license "Unlicense"
 
   def install
